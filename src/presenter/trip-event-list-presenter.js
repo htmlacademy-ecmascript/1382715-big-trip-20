acc6f955d17d80ec7f9c2c8543f6281179efd667
Nadzeya <nadzeya.zhukovich@gmail.com>
2023-05-22 15:43:17 +0200

    Added private fields and getters to classes

--- src/presenter/trip-event-list-presenter.js
+++ src/presenter/trip-event-list-presenter.js
@@ -7,33 +7,34 @@ import EventCardView from '../view/event-card-view.js';
 import DetailEventModel from '../model/detail-event-model';
 
 export default class TripEventListPresenter {
-  tripEventList = new TripEventsListView();
-
+  #tripEventList = new TripEventsListView();
+  #tripEvents = null;
+  #eventsModel = null;
   constructor({tripEvents, eventsModel}) {
-    this.tripEvents = tripEvents;
-    this.eventsModel = eventsModel;
+    this.#tripEvents = tripEvents;
+    this.#eventsModel = eventsModel;
   }
 
   init() {
-    this.events = [...this.eventsModel.getPoints()];
-    this.destinations = [...this.eventsModel.getDestinations()];
-    this.availableOffers = this.eventsModel.getOffers();
+    this.events = [...this.#eventsModel.points];
+    this.destinations = [...this.#eventsModel.destinations];
+    this.availableOffers = this.#eventsModel.offers;
 
-    render(this.tripEventList, this.tripEvents);
-    render(new TripEventItemView(), this.tripEventList.element);
+    render(this.#tripEventList, this.#tripEvents);
+    render(new TripEventItemView(), this.#tripEventList.element);
     render(
       new EventCardView({
         detailEventModel: this.createDetailEventModel(this.events[0])
       }),
-      this.tripEventList.element.querySelectorAll('.trip-events__item')[0]);
+      this.#tripEventList.element.querySelectorAll('.trip-events__item')[0]);
 
     for(let i = 1; i < this.events.length; i++) {
-      render(new TripEventItemView(), this.tripEventList.element);
+      render(new TripEventItemView(), this.#tripEventList.element);
       render(
         new EventOverviewView({
           detailEventModel:this.createDetailEventModel(this.events[i])
         }),
-        this.tripEventList.element.querySelectorAll('.trip-events__item')[i]
+        this.#tripEventList.element.querySelectorAll('.trip-events__item')[i]
       );
     }
   }
